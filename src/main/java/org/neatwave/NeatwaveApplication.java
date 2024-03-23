56803a0a76f150bbd4011cec1de9e9d895f76c4f
Kasumi Mogi <ndfz@proton.me>
2024-03-23 21:31:04 +0200

    spring initialization

---
+++ src/main/java/org/neatwave/NeatwaveApplication.java
@@ -0,0 +1,13 @@
+package org.neatwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeatwaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeatwaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/neatwave/NeatwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neatwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeatwaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
